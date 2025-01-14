a0b633899eac6a98e3355dc29c99c5dbcfe1b353
Sarthak Jariwala <sarthakjariwala@fastmail.com>
2025-01-13 22:46:07 -0800

    more detailed asserts

--- tests/test_main.py
+++ tests/test_main.py
@@ -52,6 +52,51 @@ def test_new_project_with_options(
                 ],
             )
             assert result.exit_code == 0
+
+            project_path = Path(tmpdir) / "test_project"
+
+            assert project_path.exists()
+            assert project_path.is_dir()
+
+            assert (project_path / "manage.py").exists()
+            assert (project_path / "manage.py").is_file()
+            assert (project_path / "test_project").exists()
+            assert (project_path / "test_project").is_dir()
+            assert (project_path / "test_project" / "settings.py").exists()
+            assert (project_path / "test_project" / "settings.py").is_file()
+
+            assert (project_path / "src").exists()
+            assert (project_path / "src").is_dir()
+            assert (project_path / "src" / "js").exists()
+            assert (project_path / "src" / "js").is_dir()
+            assert (project_path / "src" / "css").exists()
+            assert (project_path / "src" / "css").is_dir()
+
+            if frontend == "react":
+                assert (project_path / "src" / "js" / "main.jsx").exists()
+                assert (project_path / "src" / "js" / "main.jsx").is_file()
+            else:
+                assert (project_path / "src" / "js" / "main.js").exists()
+                assert (project_path / "src" / "js" / "main.js").is_file()
+
+            if tailwind:
+                tailwind_config_path = project_path / "tailwind.config.js"
+                postcss_config_path = project_path / "postcss.config.js"
+                assert tailwind_config_path.exists()
+                assert tailwind_config_path.is_file()
+                assert postcss_config_path.exists()
+                assert postcss_config_path.is_file()
+
+            if docker_dev:
+                assert (project_path / "dev.Dockerfile").exists()
+                assert (project_path / "dev.Dockerfile").is_file()
+                assert (project_path / "docker-compose.yaml").exists()
+                assert (project_path / "docker-compose.yaml").is_file()
+            if docker_prod:
+                assert (project_path / "prod.Dockerfile").exists()
+                assert (project_path / "prod.Dockerfile").is_file()
+                assert (project_path / "docker_startup.sh").exists()
+                assert (project_path / "docker_startup.sh").is_file()
         finally:
             os.chdir(original_dir)
 
